0bb1874790c6f3c8a4b54964efc077cd0d075bdd
Sergey <belash3@mail.ru>
2021-04-02 20:14:08 +0300

    Initial Commit

---
+++ lesson1g3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1g3
+//
+//  Created by Сергей Беляков on 02.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
